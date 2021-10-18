9305cb50b92905b9088edbd4741ca716cdeee5e8
Ryan <rvanfrachen@my.wctc.edu>
2021-10-18 13:19:41 -0500

    Searches for status in tickets only

--- Ticket-System-with-Classes-Assignment/Search.cs
+++ Ticket-System-with-Classes-Assignment/Search.cs
@@ -21,7 +21,19 @@ namespace Ticketing_System
 
             string taskFilePath = Directory.GetCurrentDirectory() + "\\task.csv";
             taskFile = new TaskFile(taskFilePath);
+            logger.Info("Created search.");
         }
 
+        public IEnumerable<String> StatusSearch(string input){
+            var output = new List<String>();
+
+            var tickets = ticketFile.Tickets.Where(Ticket => Ticket.status.Contains(input));
+            foreach(Ticket ticket in tickets){
+                output.Add(ticket.Display());
+            }
+
+            logger.Info("{Count} tickets found", tickets.Count());
+            return output.ToList();
+        }
     }
 }
