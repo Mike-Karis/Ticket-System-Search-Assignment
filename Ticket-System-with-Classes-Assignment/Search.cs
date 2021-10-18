678a8ecd202916c86076ff372aa667bfc073ca8f
Ryan <rvanfrachen@my.wctc.edu>
2021-10-18 12:31:16 -0500

    Create Search Method - Gets directories for each file - Creates a new file for each ticket

--- Ticket-System-with-Classes-Assignment/Search.cs
+++ Ticket-System-with-Classes-Assignment/Search.cs
@@ -13,7 +13,15 @@ namespace Ticketing_System
         public TaskFile taskFile;
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         public Search(){
-            
+            string ticketFilePath = Directory.GetCurrentDirectory() + "\\tickets.csv";
+            ticketFile = new TicketFile(ticketFilePath);
+
+            string enhancementFilePath = Directory.GetCurrentDirectory() + "\\enhancements.csv";
+            enhancementFile = new EnhancementFile(enhancementFilePath);
+
+            string taskFilePath = Directory.GetCurrentDirectory() + "\\task.csv";
+            taskFile = new TaskFile(taskFilePath);
         }
+
     }
 }
